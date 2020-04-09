d3a23f92093dff1046810c47cc44eb843a64bef1
Miguel Cobas <mcobas@dealergeek.com>
2020-04-09 00:25:06 -0500

    Se genero la logica para sacar el total con sus validaciones y variables

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,84 +1,115 @@
 import React, { useState } from "react";
-import styled from "@emotion/styled"
+import styled from "@emotion/styled";
+import { obtenerDiferenciaYear, calcularMarca, obtenerPlan } from "../helper";
 
 const Campo = styled.div`
-    display:flex;
-    margin-bottom: 1rem;
-    align-items:center;
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
 `;
 
-const Label =  styled.label`
-    flex: 0 0 100px;
+const Label = styled.label`
+  flex: 0 0 100px;
 `;
 
-const Select =  styled.select `
-    display:block;
-    width:100%;
-    padding:1rem;
-    border:1px solid #e1e1e1;
-    -webkit-appearance:none
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  -webkit-appearance: none;
 `;
 
-const InputRadio = styled.input `
-    margin: 0 1rem;
-`
-const Boton = styled.button `
-    background-color:#00838f;
-    font-size: 16px;
-    width: 100%;
-    padding: 1rem;
-    color: #fff;
-    text-transform: uppercase;
-    font-weight:bold;
-    border: none;
-    transition: background-color .3s ease;
-    margin-top:2rem;
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
+const Boton = styled.button`
+  background-color: #00838f;
+  font-size: 16px;
+  width: 100%;
+  padding: 1rem;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: none;
+  transition: background-color 0.3s ease;
+  margin-top: 2rem;
 
-    &:hover {
-        background-color:#36c6da;
-        cursor:pointer;
-    }
-
-`
-
+  &:hover {
+    background-color: #36c6da;
+    cursor: pointer;
+  }
+`;
+const Error = styled.div`
+  background-color: red;
+  color: white;
+  padding: 1rem;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
 const Formulario = () => {
+  const [datos, guardarDatos] = useState({
+    marca: "",
+    year: "",
+    plan: "",
+  });
 
-    const [ datos, guardarDatos] = useState({
-        marca: '',
-        year: '',
-        plan: ''
-    })
-    // extraer los valores del state 
-    const {marca, year, plan} = datos;
-const obtenerInformacion = e =>{
+  const [error, guardarError] = useState(false);
+  // extraer los valores del state
+  const { marca, year, plan } = datos;
+  const obtenerInformacion = (e) => {
     guardarDatos({
-        ...datos,
-        [e.target.name] : e.target.value
-    })
-}
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const cotizarSeguro = (e) => {
+    e.preventDefault();
+    if (marca.trim() === "" || year.trim() === "" || plan.trim() === "") {
+      guardarError(true);
+      return;
+    }
+    guardarError(false);
+
+    let resultado = 2000;
+
+    // obtener la diferencia de años
+    const diferencia = obtenerDiferenciaYear(year);
+    console.log(diferencia);
+    // por cada año hay que restar el 3%
+    resultado -= (diferencia * 3 * resultado) / 100;
+
+    // Americano 15
+    // Asiatico 5%
+    // Europeo 30%
+    resultado = calcularMarca(marca) * resultado;
+    console.log(resultado);
+
+    // Basíco aumenta 20%
+    // Completo 50%
+    const incrementoPlan = obtenerPlan(plan);
+    resultado = parseFloat(incrementoPlan * resultado).toFixed(2);
+    console.log(resultado)
+
+    // Total
+  };
   return (
-    <form>
+    <form onSubmit={cotizarSeguro}>
+      {error ? <Error>Todos los campos son obligatorios</Error> : null}
       <Campo>
         <Label>Marca</Label>
-        <Select
-            name="marca"
-            value={marca}
-            onChange={obtenerInformacion}
-        >
+        <Select name="marca" value={marca} onChange={obtenerInformacion}>
           <option value="">-- Seleccione --</option>
           <option value="Americano">Americano</option>
           <option value="Europeo">Europeo</option>
           <option value="Asiatico">Asiatico</option>
         </Select>
-        
       </Campo>
       <Campo>
-      <Label>Año</Label>
-        <Select
-            name="year"
-            value={year}
-            onChange={obtenerInformacion}
-        >
+        <Label>Año</Label>
+        <Select name="year" value={year} onChange={obtenerInformacion}>
           <option value="">-- Seleccione --</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -93,23 +124,25 @@ const obtenerInformacion = e =>{
         </Select>
       </Campo>
       <Campo>
-      <Label>Plan</Label>
-        <InputRadio 
-            type="radio"
-            name="plan"
-            value="basico"
-            checked={plan === "basico"}
-            onChange={obtenerInformacion}
-        /> Básico
-        <InputRadio 
-            type="radio"
-            name="plan"
-            value="completo"
-            checked={plan === "completo"}
-            onChange={obtenerInformacion}
-        /> Completo
+        <Label>Plan</Label>
+        <InputRadio
+          type="radio"
+          name="plan"
+          value="basico"
+          checked={plan === "basico"}
+          onChange={obtenerInformacion}
+        />{" "}
+        Básico
+        <InputRadio
+          type="radio"
+          name="plan"
+          value="completo"
+          checked={plan === "completo"}
+          onChange={obtenerInformacion}
+        />{" "}
+        Completo
       </Campo>
-      <Boton type='button'>Cotizar</Boton>
+      <Boton type="submit">Cotizar</Boton>
     </form>
   );
 };
---
+++ src/helper.js
@@ -0,0 +1,30 @@
+// obtiene la diferencia de años
+
+export function obtenerDiferenciaYear(year) {
+  return new Date().getFullYear() - year;
+}
+
+export function calcularMarca(marca) {
+  let incremento;
+
+  switch (marca) {
+    case "Europeo":
+      incremento = 1.3;
+      break;
+    case "Americano":
+      incremento = 1.15;
+      break;
+    case "Asiatico":
+      incremento = 1.05;
+      break;
+    default:
+      break;
+  }
+  return incremento;
+}
+
+// Calcular el tipo de seguro
+
+export function obtenerPlan(plan) {
+    return (plan === 'basico') ? 1.20 : 1.50
+}
